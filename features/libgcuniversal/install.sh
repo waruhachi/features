01028eed66b94080ec127a3612152d0bd860e0d1
waru <156133757+waruhachi@users.noreply.github.com>
2024-12-06 16:59:51 +0000

    [fix] remove curl

--- features/libgcuniversal/install.sh
+++ features/libgcuniversal/install.sh
@@ -2,21 +2,6 @@
 
 set -e
 
-source ./library_scripts.sh
-
-# nanolayer is a cli utility which keeps container layers as small as possible
-# source code: https://github.com/devcontainers-extra/nanolayer
-# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations,
-# and if missing - will download a temporary copy that automatically get deleted at the end
-# of the script
-ensure_nanolayer nanolayer_location "v0.5.6"
-
-# Install Curl
-$nanolayer_location \
-    install \
-    devcontainer-feature \
-    "ghcr.io/devcontainers-extra/features/curl-apt-get:1"
-
 # Install LibGcUniversal
 LIBGC_REPO_URL="https://github.com/MrGcGamer/LibGcUniversalDocumentation"
 LIBGC_TMP_DIR=$(mktemp -d)
