d9eb4afdce6c70057343017739d10d2663b71b52
waru <156133757+waruhachi@users.noreply.github.com>
2024-12-07 22:49:05 +0000

    [fix] add THEOS environment variable again

--- features/theos/install.sh
+++ features/theos/install.sh
@@ -2,21 +2,6 @@
 
 set -e
 
-source ./library_scripts.sh
-
-# nanolayer is a cli utility which keeps container layers as small as possible
-# source code: https://github.com/devcontainers-extra/nanolayer
-# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations,
-# and if missing - will download a temporary copy that automatically get deleted at the end
-# of the script
-ensure_nanolayer nanolayer_location "v0.5.6"
-
-# Install Curl
-$nanolayer_location \
-    install \
-    devcontainer-feature \
-    "ghcr.io/devcontainers-extra/features/curl-apt-get:1"
-
 # Switch to non-root user and install Theos
 sudo -iu "$_REMOTE_USER" <<EOF
     curl -fsSL https://raw.githubusercontent.com/theos/theos/master/bin/install-theos | bash
@@ -49,6 +34,11 @@ for sdk_path in $sdk_paths; do
 done
 
 echo "All SDKs have been successfully copied to $SDK_DEST_DIR."
+
+# Set Theos environment variable
+export THEOS=~/theos
+echo "export THEOS=~/theos" >> ~/.bashrc
+source ~/.bashrc
 
 echo "Cleaning up temporary files..."
 rm -rf "$SDK_TMP_DIR"
