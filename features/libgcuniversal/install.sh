55b9aa35caac4a350123a428ef1a54edf55e6066
waru <156133757+waruhachi@users.noreply.github.com>
2024-12-07 22:47:52 +0000

    [fix] possible install fix

--- features/libgcuniversal/install.sh
+++ features/libgcuniversal/install.sh
@@ -12,13 +12,8 @@ git clone "$LIBGC_REPO_URL" "$LIBGC_TMP_DIR"
 echo "Navigating to the LibGcUniversal repository directory..."
 cd "$LIBGC_TMP_DIR"
 
-if [[ -x "./install.sh" ]]; then
-    echo "Running the install.sh script..."
-    ./install.sh
-else
-    echo "install.sh script not found or not executable."
-    exit 1
-fi
+echo "Running the install.sh script..."
+./install.sh
 
 echo "Cleaning up temporary files..."
 rm -rf "$LIBGC_TMP_DIR"
