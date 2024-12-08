19f5d88324124454b2f2cf4327fccb863a7a7d2c
waru <156133757+waruhachi@users.noreply.github.com>
2024-12-08 23:09:36 +0000

    [fix] removed escaped backslash

--- features/theos/install.sh
+++ features/theos/install.sh
@@ -23,17 +23,17 @@ curl -L "$SDK_REPO_URL" -o "$SDK_TMP_DIR/repo.zip"
 echo "Extracting SDK repository..."
 unzip -q "$SDK_TMP_DIR/repo.zip" -d "$SDK_TMP_DIR"
 
-sdk_paths=\$(find "$SDK_TMP_DIR" -type d -name "*.sdk")
+sdk_paths=$(find "$SDK_TMP_DIR" -type d -name "*.sdk")
 
-if [[ -z "\$sdk_paths" ]]; then
+if [[ -z "$sdk_paths" ]]; then
     echo "No SDKs found in the repository. Exiting."
     rm -rf "$SDK_TMP_DIR"
     exit 1
 fi
 
 echo "Copying SDK directories to $SDK_DEST_DIR..."
-for sdk_path in \$sdk_paths; do
-    cp -r "\$sdk_path" "$SDK_DEST_DIR/"
+for sdk_path in $sdk_paths; do
+    cp -r "$sdk_path" "$SDK_DEST_DIR/"
 done
 
 echo "All SDKs have been successfully copied to $SDK_DEST_DIR."
