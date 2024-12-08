a78f29b90c2c7307c28e9b626a0986c4edb7d249
waru <156133757+waruhachi@users.noreply.github.com>
2024-12-08 23:52:58 +0000

    [fix] more hardcoded paths

--- features/libgcuniversal/install.sh
+++ features/libgcuniversal/install.sh
@@ -5,9 +5,6 @@ set -e
 LIBGC_REPO_URL="https://github.com/MrGcGamer/LibGcUniversalDocumentation"
 LIBGC_TMP_DIR=$(mktemp -d)
 
-# Switch to non-root user
-sudo -iu "$_REMOTE_USER" <<EOF
-
 echo "Cloning the LibGcUniversal repository..."
 git clone "$LIBGC_REPO_URL" "$LIBGC_TMP_DIR"
 
@@ -15,19 +12,19 @@ echo "Navigating to the LibGcUniversal repository directory..."
 cd "$LIBGC_TMP_DIR"
 
 echo "Copying the dynamic libraries..."
-rm -f "$THEOS/lib/libgcuniversal.dylib"
-mkdir -p "${THEOS}/lib/iphone/rootless"
-cp GcUniversal/libgcuniversal.tbd "${THEOS}/lib/"
-cp GcUniversal/libgcuniversalrootless.tbd "${THEOS}/lib/iphone/rootless/libgcuniversal.tbd"
+rm -f "/home/vscode/lib/libgcuniversal.dylib"
+mkdir -p "/home/vscode/lib/iphone/rootless"
+cp GcUniversal/libgcuniversal.tbd "/home/vscode/lib/"
+cp GcUniversal/libgcuniversalrootless.tbd "/home/vscode/lib/iphone/rootless/libgcuniversal.tbd"
 
 echo "Copying the header files..."
-mkdir -p "${THEOS}/include/GcUniversal"
-cp GcUniversal/*.h "${THEOS}/include/GcUniversal/"
+mkdir -p "/home/vscode/include/GcUniversal"
+cp GcUniversal/*.h "/home/vscode/include/GcUniversal/"
 
 echo "Copying the module map..."
-cp GcUniversal/module.modulemap "${THEOS}/include/GcUniversal/"
+cp GcUniversal/module.modulemap "/home/vscode/include/GcUniversal/"
 
-EOF
+echo "Installation successful!"
 
 echo "Cleaning up temporary files..."
-rm -rf "$LIBGC_TMP_DIR"
+rm -rf "$LIBGC_TMP_DIR"[fix]
