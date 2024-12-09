d8e436afea3fede29527fb8bff4d0662a030e76d
waru <156133757+waruhachi@users.noreply.github.com>
2024-12-09 08:31:27 +0000

    [fix] properly find the workspace path

--- features/remotelog/install.sh
+++ features/remotelog/install.sh
@@ -2,15 +2,16 @@
 
 set -e
 
+echo "Adding THEOS environment variable..."
+export THEOS=/home/vscode/theos
+echo "export THEOS=/home/vscode/theos" >> ~/.bashrc
+source ~/.bashrc
+
 RL_REPO_URL="https://github.com/Muirey03/RemoteLog.git"
 RL_DEST_DIR="$THEOS/include"
 RL_TMP_DIR=$(mktemp -d)
 RL_IP_ADDRESS=$(curl -s ifconfig.me)
-WORKSPACE_DIR=$(find /workspaces -mindepth 1 -maxdepth 1 -type d | head -n 1)
-
-echo "Adding THEOS environment variable..."
-echo "export THEOS=/home/vscode/theos" >> ~/.bashrc
-source ~/.bashrc
+WORKSPACE_DIR=$(find /workspaces -mindepth 1 -maxdepth 1 -type d ! -name ".*" | head -n 1)
 
 echo "Cloning the RemoteLog repository..."
 git clone "$RL_REPO_URL" "$RL_TMP_DIR"
@@ -24,22 +25,14 @@ sed -i "s/replace with ip/$RL_IP_ADDRESS/" RemoteLog.h
 echo "Copying RemoteLog.h to $RL_DEST_DIR..."
 cp RemoteLog.h $RL_DEST_DIR
 
-echo "Moving RemoteLog server to workspace directory..."
 echo $WORKSPACE_DIR
-echo ${localWorkspaceFolder}
-echo ${containerWorkspaceFolder}
-echo ${localWorkspaceFolderBasename}
-echo ${containerWorkspaceFolderBasename}
-
-# if [ -n "$localWorkspaceFolder" ]; then
-#     echo "Moving RemoteLog server to workspace directory: $localWorkspaceFolder"
-#     mv rlogserver.py "$localWorkspaceFolder"
+# if [ -n "$WORKSPACE_DIR" ]; then
+#     echo "Moving RemoteLog server to workspace directory: $WORKSPACE_DIR"
+#     mv rlogserver.py "$WORKSPACE_DIR"
 # else
 #     echo "Error: No directories found in /workspaces."
 #     exit 1
 # fi
-
-# Run the python server on your computer and watch the logs coming in :)
 
 echo "Cleaning up temporary files..."
 rm -rf "$RL_TMP_DIR"
