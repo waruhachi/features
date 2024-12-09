ecb04b5141236e507c345e56b3c299dcaca0fd11
waru <156133757+waruhachi@users.noreply.github.com>
2024-12-09 08:45:06 +0000

    [fix] move server to user home instead

--- features/remotelog/install.sh
+++ features/remotelog/install.sh
@@ -11,7 +11,6 @@ RL_REPO_URL="https://github.com/Muirey03/RemoteLog.git"
 RL_DEST_DIR="$THEOS/include"
 RL_TMP_DIR=$(mktemp -d)
 RL_IP_ADDRESS=$(curl -s ifconfig.me)
-WORKSPACE_DIR=$(find /workspaces -mindepth 1 -maxdepth 1 -type d ! -name ".*" | head -n 1)
 
 echo "Cloning the RemoteLog repository..."
 git clone "$RL_REPO_URL" "$RL_TMP_DIR"
@@ -25,13 +24,8 @@ sed -i "s/replace with ip/$RL_IP_ADDRESS/" RemoteLog.h
 echo "Copying RemoteLog.h to $RL_DEST_DIR..."
 cp RemoteLog.h $RL_DEST_DIR
 
-if [ -n "$WORKSPACE_DIR" ]; then
-    echo "Moving RemoteLog server to workspace directory: $WORKSPACE_DIR"
-    mv rlogserver.py "$WORKSPACE_DIR"
-else
-    echo "Error: No directories found in /workspaces."
-    exit 1
-fi
+echo "Moving RemoteLog server to user home directory..."
+mv rlogserver.py /home/vscode/
 
 echo "Cleaning up temporary files..."
 rm -rf "$RL_TMP_DIR"
