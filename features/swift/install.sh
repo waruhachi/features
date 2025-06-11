4795273a709ce9273eb6dd3d2bd9723c6b311781
waru <156133757+waruhachi@users.noreply.github.com>
2025-06-11 21:37:10 +0000

    [fix] streamline Swift installation script and update environment setup

--- features/swift/install.sh
+++ features/swift/install.sh
@@ -10,16 +10,15 @@ curl -O https://download.swift.org/swiftly/linux/swiftly-$(uname -m).tar.gz
 tar zxf swiftly-$(uname -m).tar.gz
 ./swiftly init --quiet-shell-followup --assume-yes
 
-echo "Updating PATH..."
+echo "Setting up Swift environment..."
+. "${SWIFTLY_HOME_DIR:-~/.local/share/swiftly}/env.sh"
+hash -r
 
-export SWIFTLY_HOME_DIR="/home/vscode/.local/share/swiftly"
-export SWIFTLY_BIN_DIR="/home/vscode/.local/share/swiftly/bin"
-if [[ ":$PATH:" != *":$SWIFTLY_BIN_DIR:"* ]]; then
-    export PATH="$SWIFTLY_BIN_DIR:$PATH"
-fi
+echo "Adding Swift to PATH..."
+echo '. "${SWIFTLY_HOME_DIR:-~/.local/share/swiftly}/env.sh"' >> $VSCODE_HOME/.bashrc
 
 echo "Cleaning up..."
-rm -f LICENSE.txt
 rm -f swiftly-$(uname -m).tar.gz
 
 echo "Swift has been installed successfully via swiftly."
+echo "Swift toolchain is now available in your PATH."
