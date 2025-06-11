95aa83943a9b49a3c961c16b03fc4e88a3f3947b
waru <156133757+waruhachi@users.noreply.github.com>
2025-06-11 21:07:44 +0000

    [fix] update Swift version to 6.1.2 and streamline installation script

--- features/swift/install.sh
+++ features/swift/install.sh
@@ -3,44 +3,27 @@
 set -e
 
 VSCODE_HOME=/home/vscode
-SWIFT_VERSION="6.0.2"
-SWIFT_PLATFORM="ubuntu2204"
-SWIFT_PLATFORM_VERSION="ubuntu22.04"
-SWIFT_TMP_DIR=$(mktemp -d)
 
-echo "Installing Swift dependencies..."
+echo "Installing dependencies..."
 
-sudo apt-get update
-sudo apt-get install -y \
-    binutils \
-    git \
-    gnupg2 \
-    libc6-dev \
-    libcurl4-openssl-dev \
-    libedit2 \
-    libgcc-11-dev \
-    libpython3-dev \
-    libsqlite3-0 \
-    libstdc++-11-dev \
-    libxml2-dev \
-    libz3-dev \
-    pkg-config \
-    python3-lldb-13 \
-    tzdata \
-    unzip \
-    zlib1g-dev
+sudo apt-get -y install libpython3-dev libxml2-dev libz3-dev pkg-config python3-lldb-13
 
-echo "Downloading Swift $SWIFT_VERSION for $SWIFT_PLATFORM_VERSION..."
-curl -L "https://download.swift.org/swift-$SWIFT_VERSION-release/$SWIFT_PLATFORM/swift-$SWIFT_VERSION-RELEASE/swift-$SWIFT_VERSION-RELEASE-$SWIFT_PLATFORM_VERSION.tar.gz" -o "$SWIFT_TMP_DIR/swift-$SWIFT_VERSION-$SWIFT_PLATFORM_VERSION.tar.gz"
+echo "Installing Swift using swiftly..."
 
-echo "Extracting Swift archive..."
-tar xzf "$SWIFT_TMP_DIR/swift-$SWIFT_VERSION-$SWIFT_PLATFORM_VERSION.tar.gz" -C "$VSCODE_HOME/"
-mv "$VSCODE_HOME/swift-$SWIFT_VERSION-RELEASE-$SWIFT_PLATFORM_VERSION" "$VSCODE_HOME/Swift"
+curl -O https://download.swift.org/swiftly/linux/swiftly-$(uname -m).tar.gz
+tar zxf swiftly-$(uname -m).tar.gz
+./swiftly init --quiet-shell-followup --assume-yes
 
-echo "Adding Swift to PATH..."
-echo "export PATH=\"$VSCODE_HOME/Swift/usr/bin:\$PATH\"" >> $VSCODE_HOME/.bashrc
-source $VSCODE_HOME/.bashrc
-echo "Swift $SWIFT_VERSION has been installed successfully."
+echo "Updating PATH..."
 
-echo "Cleaning up temporary files..."
-rm -rf "$SWIFT_TMP_DIR"
+export SWIFTLY_HOME_DIR="/home/vscode/.local/share/swiftly"
+export SWIFTLY_BIN_DIR="/home/vscode/.local/share/swiftly/bin"
+if [[ ":$PATH:" != *":$SWIFTLY_BIN_DIR:"* ]]; then
+    export PATH="$SWIFTLY_BIN_DIR:$PATH"
+fi
+
+echo "Cleaning up..."
+rm -f LICENSE.txt
+rm -f swiftly-$(uname -m).tar.gz
+
+echo "Swift has been installed successfully via swiftly."
